6947ca12107050b0874e86875322309b68ce16e8
523ChloeL <92735830+523ChloeL@users.noreply.github.com>
2023-05-15 13:48:32 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -23,14 +23,14 @@ function changeNavbarColor() {
     var home = document.getElementById("home")
     if (scrollPos > 0.5) {
         navbar.style.backgroundColor = '#0A2C64'
-        home.style.color = "#F8EBF0"
-        document.getElementById("about-us").style.color = "#F8EBF0"
-        document.getElementById("organizations").style.color = "#F8EBF0"
-        document.getElementById("tutorials").style.color = "#F8EBF0"
-        document.getElementById("instagram").style.color = "#F8EBF0"
-        document.getElementById("contact").style.color = "#F8EBF0"
+        home.style.color = "#2a2a2a"
+        document.getElementById("about-us").style.color = "#2a2a2a"
+        document.getElementById("organizations").style.color = "#2a2a2a"
+        document.getElementById("tutorials").style.color = "#2a2a2a"
+        document.getElementById("instagram").style.color = "#2a2a2a"
+        document.getElementById("contact").style.color = "#2a2a2a"
     } else {
-        navbar.style.backgroundColor = '#F8EBF0'
+        navbar.style.backgroundColor = '#2a2a2a'
         document.getElementById("home").style.color = "#0A2C64"
         document.getElementById("about-us").style.color = "#0A2C64"
         document.getElementById("organizations").style.color = "#0A2C64"
